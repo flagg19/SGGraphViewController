6c365e652d5803320cd54ff34907d38296611d27
michele.amati <wmacosx@gmail.com>
2012-05-17 12:27:06 +0200

    First commit, loading empty project

---
+++ SGGraphViewController/SGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SGAppDelegate.h
+//  SGGraphViewController
+//
+//  Created by Michele Amati on 5/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
